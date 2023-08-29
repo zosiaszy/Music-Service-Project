7b01baa9fb58983b7a9946159d2303b0318a10d8
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-08-29 14:20:07 +0200

    Add categories to music service

---
+++ src/js/script.js
@@ -0,0 +1,183 @@
+/* global Handlebars, GreenAudioPlayer */
+
+const templates = {
+  songTemplate: Handlebars.compile(document.getElementById('template-song').innerHTML),
+  searchTemplate: Handlebars.compile(document.getElementById('template-search').innerHTML),
+  discoverTemplate: Handlebars.compile(document.getElementById('template-discover').innerHTML),
+  navigationTemplate: Handlebars.compile(document.getElementById('template-navigation').innerHTML),
+  categoriesTemplate: Handlebars.compile(document.getElementById('template-categories').innerHTML),
+};
+
+class MusicServiceApp {
+  
+  constructor() {
+    const thisMusicService = this;
+    thisMusicService.pages = document.querySelectorAll('#pages section'); 
+    thisMusicService.navLinks = document.querySelectorAll('.navbar a'); 
+    thisMusicService.songView = document.getElementById('song-view');
+    thisMusicService.appUrl = '../db/app.json';
+        
+    thisMusicService.initPages();
+    thisMusicService.initSongs();
+    thisMusicService.initSearchForm();
+    thisMusicService.renderCategories();
+    thisMusicService.initCategories();
+
+    const searchForm = document.getElementById('search-form');
+    searchForm.addEventListener('submit', function(event) {
+      event.preventDefault();
+      thisMusicService.searchSongs();
+    });
+  }
+
+  initPages() {
+    const thisMusicService = this;
+    for (let link of thisMusicService.navLinks) {
+      link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const pageId = link.getAttribute('href').replace('#', '');
+        thisMusicService.activatePage(pageId);
+      });  
+    }
+
+   
+    const idFromHash = window.location.hash.replace('#/', '');
+    let pageMatchingHash = thisMusicService.pages[0].id;
+
+    for (let page of this.pages) {
+      if (page.id === idFromHash) {
+        pageMatchingHash = page.id;
+        
+        break;
+      }
+    }
+    thisMusicService.activatePage(pageMatchingHash);
+
+   
+
+
+  }
+
+  activatePage(pageId) {
+    const thisMusicService = this;
+    for (let page of thisMusicService.pages) {
+      page.classList.toggle('active', page.id === pageId);
+    }
+
+    for (let link of thisMusicService.navLinks) {
+      link.classList.toggle('active', link.getAttribute('href') === '#' + pageId);
+    }
+
+    
+  }
+  
+
+  initSongs() {
+    const thisMusicService = this;
+    fetch(this.appUrl)
+      .then(response => response.json())
+      .then(appData => {
+        for (let eachSong of appData.songs) {
+          const songHTML = templates.songTemplate(eachSong);
+          thisMusicService.songView.innerHTML += songHTML;
+        }
+        GreenAudioPlayer.init({
+          selector: '.player',
+          stopOthersOnPlay: true
+        });
+      });
+  }
+  
+  initSearchForm() {
+    const thisMusicService = this;
+    thisMusicService.searchInput = document.getElementById('search-input');
+    thisMusicService.categoryInput = document.getElementById('category-input');
+    thisMusicService.searchResults = document.getElementById('search-results');
+  }
+
+
+  searchSongs() {
+    const thisMusicService = this;
+    const searchValue = thisMusicService.searchInput.value;
+    
+    fetch(thisMusicService.appUrl)
+      .then(response => response.json())
+      .then(appData => {
+        thisMusicService.songView.innerHTML = '';
+        
+        for (let eachSong of appData.songs) {
+          if (eachSong.title.toLowerCase().includes(searchValue)) {
+            const songHTML = templates.songTemplate(eachSong);
+            thisMusicService.searchResults.innerHTML = songHTML;
+            break; 
+          }
+        }
+
+        GreenAudioPlayer.init({
+          selector: '.player',
+          stopOthersOnPlay: true
+        });
+      });
+  }
+
+  renderCategories() {
+    const thisMusicService = this;
+    const categoriesContainer = document.querySelector('.categories-container');
+
+    fetch(thisMusicService.appUrl)
+      .then(response => response.json())
+      .then(appData => {
+        const categoriesTemplateHTML = templates.categoriesTemplate({
+          categories: appData.categories
+        });
+        categoriesContainer.innerHTML = categoriesTemplateHTML;
+
+        thisMusicService.initCategories();
+      });
+  }
+
+
+  initCategories(){
+    const thisMusicService = this;
+
+    const categoriesLinks = document.querySelectorAll('.category-link');
+
+    for(let link of categoriesLinks){
+      link.addEventListener('click', function(){
+        const selectedCategory = link.getAttribute('data-category');
+        thisMusicService.filterCategories(selectedCategory);
+     
+
+      });
+
+     
+    }
+
+  }
+
+  filterCategories(selectedCategory){
+    const thisMusicService = this;
+
+    const allSongs = thisMusicService.songView.querySelectorAll('.song-container');
+
+    for(let song of allSongs){
+
+      const songCategories = song.getAttribute('data-categories').split(' ');
+
+      if(songCategories.includes(selectedCategory)){
+        song.classList.remove('hidden');
+      } else{
+        song.classList.add('hidden');
+      }
+
+    }
+
+  }
+
+ 
+
+
+}
+
+new MusicServiceApp();
+
