6c84be948cefd236e20a5b29fbc4f6513f9ed95c
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-09-02 11:38:56 +0200

    Add changes for discover

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+
+
 /* global Handlebars, GreenAudioPlayer */
 
 const templates = {
@@ -6,6 +8,7 @@ const templates = {
   discoverTemplate: Handlebars.compile(document.getElementById('template-discover').innerHTML),
   navigationTemplate: Handlebars.compile(document.getElementById('template-navigation').innerHTML),
   categoriesTemplate: Handlebars.compile(document.getElementById('template-categories').innerHTML),
+  
 };
 
 class MusicServiceApp {
@@ -16,20 +19,25 @@ class MusicServiceApp {
     thisMusicService.navLinks = document.querySelectorAll('.navbar a'); 
     thisMusicService.songView = document.getElementById('song-view');
     thisMusicService.appUrl = '../db/app.json';
-
-    thisMusicService.categoryList = [];
-        
+    
+    
+    
     thisMusicService.initPages();
     thisMusicService.initSongs();
     thisMusicService.initSearchForm();
     thisMusicService.renderCategories();
     thisMusicService.initCategories();
-
+    
+    
+    
     const searchForm = document.getElementById('search-form');
     searchForm.addEventListener('submit', function(event) {
       event.preventDefault();
       thisMusicService.searchSongs();
     });
+
+    thisMusicService.initDiscover();
+    
   }
 
   initPages() {
@@ -85,10 +93,8 @@ class MusicServiceApp {
           const songHTML = templates.songTemplate(eachSong);
           thisMusicService.songView.innerHTML += songHTML;
         }
-        GreenAudioPlayer.init({ 
-          selector: '.player', 
-          stopOthersOnPlay: true
-        });
+        
+        thisMusicService.initPlayer();
       });
   }
   
@@ -109,7 +115,7 @@ class MusicServiceApp {
     fetch(thisMusicService.appUrl)
       .then(response => response.json())
       .then(appData => {
-        thisMusicService.searchResults.innerHTML = ''; // Clear previous search results
+        thisMusicService.searchResults.innerHTML = '';
         
         for (let eachSong of appData.songs) {
           if (
@@ -117,18 +123,22 @@ class MusicServiceApp {
             (selectCategory === 'all' || eachSong.categories.includes(selectCategory))
           ) {
             const songHTML = templates.songTemplate(eachSong);
-            thisMusicService.searchResults.innerHTML += songHTML; // Append to search results
+            thisMusicService.searchResults.innerHTML += songHTML; 
           }
         }
+        thisMusicService.initPlayer();
+     
         
-        GreenAudioPlayer.init({
-          selector: '.player',
-          stopOthersOnPlay: true
-        });
       });
   }
 
- 
+  initPlayer (){
+    GreenAudioPlayer.init({ 
+      selector: '.player', 
+      stopOthersOnPlay: true
+    });
+    
+  }
 
   renderCategories() {
     const thisMusicService = this;
@@ -191,11 +201,53 @@ class MusicServiceApp {
     }
   }
 
-  
+  initDiscover() {
+    const thisMusicService = this;
+    const discoverPlaylist = document.querySelector('.discover-playlist-wrapper');
 
- 
+    fetch(thisMusicService.appUrl)
+      .then(response => response.json())
+      .then(appData => {
+        const playedSongs = appData.songs.filter(song => song.ranking > 0);
+        const discoverInstance = new Discover(playedSongs, discoverPlaylist);
+        discoverInstance.renderRandomSong(); 
+      });
+  }
+}
+class Discover {
+  constructor(playedSongs, playlistWrapper) {
+    this.playlistWrapper = playlistWrapper;
+    this.playedSongs = playedSongs;
+    this.currentSongIndex = 0;
+  }
 
+  createAudioElement(song) {
+    const audioElement = document.createElement('audio');
+    audioElement.src = `songs/${song.filename}`;
+    return audioElement;
+  }
 
+  renderRandomSong() {
+    if (this.playedSongs.length > 0) {
+      const randomIndex = Math.floor(Math.random() * this.playedSongs.length);
+      const randomSong = this.playedSongs[randomIndex];
+
+      const containerOfSong = document.createElement('div');
+      containerOfSong.classList.add('discover-song');
+      containerOfSong.setAttribute('data-id', randomSong.id);
+
+      const audioElement = this.createAudioElement(randomSong);
+      containerOfSong.appendChild(audioElement);
+
+      this.playlistWrapper.innerHTML = ''; 
+      this.playlistWrapper.appendChild(containerOfSong);
+
+      GreenAudioPlayer.init({
+        selector: '.discover-song',
+        stopOthersOnPlay: true,
+      });
+    } 
+  }
 }
 
 new MusicServiceApp();
